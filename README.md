834b9e5af62b61c52d3a772566e8e7910c7417ac
Bjorn Borud <borud@lab5e.com>
2022-06-02 17:29:33 +0200

    added badge and example in godoc

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This repository contains a simple DTLS client library for communicating with Span as an IoT device. If you want to access the Span API for managing Span resources, the library you want is the [Go Span API Client](https://github.com/lab5e/go-spanapi).
 
+[![Godoc](http://img.shields.io/badge/godoc-reference-blue.svg?style=flat)](https://pkg.go.dev/github.com/borud/go-span-client)
+
 ## Minimal example
 
 ```go
--- client.go
+++ client.go
@@ -1,3 +1,36 @@
+// Package client implements a DTLS based client for Span
+//
+// Sample usage:
+//     package main
+//
+//     import (
+//         "log"
+//         "time"
+//         client "github.com/borud/go-span-client"
+//     )
+//
+//     func main() {
+//         client, err := client.Connect(client.NewDefaultConfig())
+//         if err != nil {
+//             log.Fatal(err)
+//         }
+//         defer client.Close()
+//
+//         n, err := client.Write([]byte("this is a test"), time.Second)
+//         if err != nil {
+//             log.Fatal(err)
+//         }
+//         log.Printf("wrote %d bytes", n)
+//
+//         buffer := make([]byte, 1024)
+//
+//         n, err = client.Read(buffer, time.Second*5)
+//         if err != nil {
+//             log.Fatal(err)
+//         }
+//         log.Printf("read %d bytes: [%s]", n, string(buffer))
+//     }
+///
 package client
 
 import (
